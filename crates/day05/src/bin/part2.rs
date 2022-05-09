805550b344a70c8c43a22670de55435301903df8
Euan <euan@rochester.me.uk>
2022-05-09 11:50:21 +0100

    Day 05 part 2

--- crates/day05/src/bin/part2.rs
+++ crates/day05/src/bin/part2.rs
@@ -1,13 +1,51 @@
-use std::io::BufRead;
+use std::{io::BufRead, collections::HashSet};
 
-fn solution(input: impl BufRead) -> shared::Result<u32> {
-    todo!()
+use shared::parameterized_tests;
+
+fn has_nonoverlapping_repeated_pair(line: &str) -> bool{
+    for i in 0..line.chars().count(){
+        let pair = (line.chars().nth(i),line.chars().nth(i+1));
+        let pair = if let (Some(a),Some(b)) = pair{
+            (a,b)
+        } else {
+            break
+        };
+        let mut following_pairs = line.chars().skip(i+2).zip(line.chars().skip(i+3));
+        if following_pairs.any(|x| x == pair){
+            return true;
+        }
+    }
+    false
+}
+
+fn has_skipped_repeat(line: &str) -> bool{
+    for (a,c) in line.chars().zip(line.chars().skip(2)) {
+        if a == c {
+            return true;
+        }
+    }
+    return false;
+}
+
+fn solution(input: impl BufRead) -> shared::Result<i32> {
+    let mut count = 0;
+    for line in input.lines(){
+        let line = line?;
+        let is_nice = has_nonoverlapping_repeated_pair(&line) 
+            && has_skipped_repeat(&line);
+        if is_nice{
+            count += 1;
+        }
+    }
+
+    Ok(count)
 }
 
 shared::main!(solution);
 
-#[cfg(test)]
-#[test]
-fn day05_part2_example() {
-    shared::check_example(solution, todo!(), todo!())
+parameterized_tests! {
+    day05_part2_example_a: solution("qjhvhtzxzqqjkmpb") == 1,
+    day05_part2_example_b: solution("xxyxx") == 1,
+    day05_part2_example_c: solution("uurcxstgmygtbstg") == 0,
+    day05_part2_example_d: solution("ieodomkazucvgmuy") == 0,
 }
