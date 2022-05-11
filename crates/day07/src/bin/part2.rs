f1339e41f2e841c8be68b4740961ba5fee28a7f6
Euan <euan@rochester.me.uk>
2022-05-11 18:46:58 +0100

    Day 07 part2

--- crates/day07/src/bin/part2.rs
+++ crates/day07/src/bin/part2.rs
@@ -1,13 +1,22 @@
 use std::io::BufRead;
 
-fn solution(input: impl BufRead) -> shared::Result<u32> {
-    todo!()
+use day07::*;
+use shared::{combine::EasyParser, anyhow};
+
+fn solution(mut input: impl BufRead) -> shared::Result<u16> {
+    let mut buf = String::new();
+    input.read_to_string(&mut buf)?;
+    let (mut instructions, _rest) = Instructions::parser()
+        .easy_parse(shared::combine::stream::position::Stream::new(&*buf))
+        .map_err(|err| anyhow!(err.map_range(|s| s.to_string())))?;
+
+    let value = WireName::from("a").evaluate(&mut EvaluationContext::new(), &instructions).expect("No value on wire a!");
+
+    instructions.override_wire(WireName::from("b"), value);
+
+    let value = WireName::from("a").evaluate(&mut EvaluationContext::new(), &instructions).expect("No value on wire a!");
+
+    Ok(value)
 }
 
 shared::main!(solution);
-
-#[cfg(test)]
-#[test]
-fn day07_part2_example() {
-    shared::check_example(solution, todo!(), todo!())
-}
